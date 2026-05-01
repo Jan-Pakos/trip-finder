65f7a0b19a6025ee002fad87a17d0d63c8af2c76
Jan-Pakos <janpakos123@gmail.com>
2026-05-01 16:09:52 +0200

    added flightreservationresponse dto

---
+++ src/main/java/com/jan/trip_finder/dto/FlightReservationResponse.java
@@ -0,0 +1,14 @@
+package com.jan.trip_finder.dto;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+public record FlightReservationResponse(
+        UUID reservationId,
+        String departure,
+        String arrival,
+        String flightNumber,
+        LocalDate tripDate,
+        int price
+) {
+}
