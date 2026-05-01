eb89cb4376aa1764a28738ec7b52d4fadae9b010
Jan-Pakos <janpakos123@gmail.com>
2026-05-01 12:32:16 +0200

    Initial commit

---
+++ src/main/java/com/jan/trip_finder/TripFinderApplication.java
@@ -0,0 +1,13 @@
+package com.jan.trip_finder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jan/trip_finder/TripFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jan.trip_finder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
